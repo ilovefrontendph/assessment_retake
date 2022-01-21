e54e163abb7816fba923bad155c0058a438c0b46
mckeenasma <50982785+mckeenasma@users.noreply.github.com>
2022-01-22 02:29:53 +0800

    firebase deploy

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="flex flex-col justify-center items-center h-screen w-screen">
+      <p>Phone: 0932390001 </p>
+      <p>Address: UIC BANGKEROHAN</p>
+      <iframe
+        width="600"
+        height="500"
+        id="gmap_canvas"
+        src="https://maps.google.com/maps?ll=28.5449756,77.1904397&q=Indian Institute of Technology Delhi&t=&z=14&ie=UTF8&iwloc=&output=embed"
+        frameborder="0"
+        scrolling="no"
+        marginheight="0"
+        marginwidth="0"
+      ></iframe>
+    </div>
+  );
 }
 
 export default Contact;
--- src/pages/tech/TechStack.js
+++ src/pages/tech/TechStack.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function TechStack() {
-  return <div>TechStack</div>;
+  return (
+    <div className="flex flex-col justify-center items-center h-screen w-screen">
+      <p>Reactjs</p>
+      <p>Firebase</p>
+      <p>Tailwindcss</p>
+      <p>Npm Packages</p>
+    </div>
+  );
 }
 
 export default TechStack;
